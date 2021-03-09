6bbbba60d20a26a4c7f8ec718d4569cfc037ccca
muramidaza <muramidaza@gmail.com>
2021-03-09 11:38:13 +0300

    fix font messages

--- tmpl/error.php
+++ tmpl/error.php
@@ -1,3 +1,4 @@
-<div style="font-size: 12px; font-weight: bold; color: #949494;">
+<div style="font-size: 14px; font-weight: bold; color: #949494;">
+    <hr>
 	<?php echo $params['feedback_error_message']?>
 </div>
